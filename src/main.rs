deeda32c7a1547c5d4de25aa0d20f3ed605aa0e7
Richard Jonas <richard.jonas.76@gmail.com>
2025-10-15 17:31:41 +0200

    Scale names and fix modes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use pitch::{Pitch, PitchClass};
-use scale::display_scale;
 
 use crate::scale::Scale;
 
@@ -11,6 +10,33 @@ pub mod pitch;
 pub mod scale;
 
 fn main() {
-    display_scale(&Scale::new(&Pitch::new(PitchClass::C), &scale::IONIAN).pitches);
-    display_scale(&Scale::new(&Pitch::new(PitchClass::E), &scale::IONIAN).pitches);
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "ionian", &scale::IONIAN)
+    );
+
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "dorian", &scale::DORIAN)
+    );
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "phrygian", &scale::PHRYGIAN)
+    );
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "lydian", &scale::LYDIAN)
+    );
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "mixolydian", &scale::MIXOLYDIAN)
+    );
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "aeolian", &scale::AEOLIAN)
+    );
+    println!(
+        "{}",
+        Scale::new(&Pitch::new(PitchClass::E), "locrian", &scale::LOCRIAN)
+    );
 }
--- src/scale.rs
+++ src/scale.rs
@@ -1,4 +1,6 @@
-use crate::pitch::{Accidental, Pitch, PitchClass};
+use std::fmt::Display;
+
+use crate::pitch::{Accidental, Pitch};
 
 #[derive(Clone, Copy, Debug)]
 pub struct ScaleDegree {
@@ -9,6 +11,7 @@ pub struct ScaleDegree {
 #[derive(Clone, Debug)]
 pub struct Scale {
     pub root_note: Pitch,
+    pub name: String,
     pub scale_degrees: Vec<ScaleDegree>,
     pub pitches: Vec<Pitch>,
 }
@@ -44,27 +47,227 @@ pub const IONIAN: [ScaleDegree; 7] = [
     },
 ];
 
+pub const DORIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Flat,
+    },
+];
+
+pub const PHRYGIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Flat,
+    },
+];
+
+pub const LYDIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Sharp,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Natural,
+    },
+];
+
+pub const MIXOLYDIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Flat,
+    },
+];
+
+pub const AEOLIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Flat,
+    },
+];
+
+pub const LOCRIAN: [ScaleDegree; 7] = [
+    ScaleDegree {
+        degree: 1,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 2,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 3,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 4,
+        modifier: Accidental::Natural,
+    },
+    ScaleDegree {
+        degree: 5,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 6,
+        modifier: Accidental::Flat,
+    },
+    ScaleDegree {
+        degree: 7,
+        modifier: Accidental::Flat,
+    },
+];
+
 /// Interval distances of scale degrees.
-pub const SCALE_DEGREES: [u8; 7] = [0, 2, 4, 5, 7, 9, 11];
+pub const SCALE_NATURAL_DEGREES: [u8; 7] = [0, 2, 4, 5, 7, 9, 11];
 
 impl Scale {
-    pub fn new(root_note: &Pitch, degrees: &[ScaleDegree]) -> Self {
+    pub fn new(root_note: &Pitch, name: &str, degrees: &[ScaleDegree]) -> Self {
         Scale {
             root_note: *root_note,
+            name: name.to_string(),
             scale_degrees: degrees.to_vec(),
             pitches: Scale::build_scale(root_note, degrees),
         }
+    }
+
+    pub fn to_numeric_degrees(degrees: &[ScaleDegree]) -> Vec<i8> {
+        let mut result = vec![0];
+
+        for i in 1..degrees.len() {
+            let nd = SCALE_NATURAL_DEGREES[i] as i8 + degrees[i].modifier as i8;
+
+            result.push(nd);
+        }
+
+        result
     }
 
     pub fn build_scale(root_note: &Pitch, degrees: &[ScaleDegree]) -> Vec<Pitch> {
         let mut result = vec![*root_note];
         let mut i = 1;
         let mut last_pitch = *root_note;
-        let mut last_degree = SCALE_DEGREES[0] as i8;
+        let mut last_degree = 0i8;
+        let numeric_degrees = Scale::to_numeric_degrees(degrees);
 
         loop {
             let next_degree = degrees[i];
-            let next_num_degree = SCALE_DEGREES[(next_degree.degree - 1) as usize] as i8;
+            let next_num_degree = numeric_degrees[(next_degree.degree - 1) as usize];
             let half_notes = next_num_degree - last_degree;
             let next_pitch = last_pitch.build_next(half_notes);
 
@@ -82,85 +285,48 @@ impl Scale {
     }
 }
 
-//pub const DORIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Major2nd,
-//    Interval::Minor3rd,
-//    Interval::Perfect4th,
-//    Interval::Perfect5th,
-//    Interval::Major6th,
-//    Interval::Minor7th,
-//];
+impl Display for ScaleDegree {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}{}", self.degree, self.modifier))
+    }
+}
 
-//pub const PHRYGIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Minor2nd,
-//    Interval::Minor3rd,
-//    Interval::Perfect4th,
-//    Interval::Perfect5th,
-//    Interval::Minor6th,
-//    Interval::Minor7th,
-//];
-//
-//// I still don't have a good modeling idea how to express the flat 4th here.
-//pub const LYDIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Major2nd,
-//    Interval::Major3rd,
-//    Interval::Tritone,
-//    Interval::Perfect5th,
-//    Interval::Major6th,
-//    Interval::Major7th,
-//];
-//
-//pub const MIXOLYDIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Major2nd,
-//    Interval::Major3rd,
-//    Interval::Perfect4th,
-//    Interval::Perfect5th,
-//    Interval::Major6th,
-//    Interval::Minor7th,
-//];
-//
-//pub const AEOLIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Major2nd,
-//    Interval::Minor3rd,
-//    Interval::Perfect4th,
-//    Interval::Perfect5th,
-//    Interval::Minor6th,
-//    Interval::Minor7th,
-//];
-//
-//pub const LOCRIAN: [Interval; 7] = [
-//    Interval::Unison,
-//    Interval::Minor2nd,
-//    Interval::Minor3rd,
-//    Interval::Perfect4th,
-//    Interval::Tritone,
-//    Interval::Minor6th,
-//    Interval::Minor7th,
-//];
+impl Display for Scale {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let _ = f.write_fmt(format_args!("{} {} - ", self.root_note, self.name));
+
+        for pitch in &self.pitches {
+            let _ = f.write_fmt(format_args!("{} ", pitch));
+        }
+
+        let _ = f.write_str(" - ");
+
+        for degree in &self.scale_degrees {
+            let _ = f.write_fmt(format_args!("{} ", degree));
+        }
+
+        Ok(())
+    }
+}
 
 // TODO implement a feature like, one F lydian I want chord progression:
 // I iv V III or something
 
 // Here we need to learn const generic
 
-pub fn compute_scale(root_note: &Pitch, scale: &[ScaleDegree; 7]) -> Vec<Pitch> {
-    let mut new_scale = vec![];
-
-    for (i, degree) in scale.iter().enumerate() {
-        let distance = SCALE_DEGREES[i];
-
-        if distance == 0 {
-            new_scale.push(root_note.clone());
-        }
-    }
-
-    new_scale
-}
+//pub fn compute_scale(root_note: &Pitch, scale: &[ScaleDegree; 7]) -> Vec<Pitch> {
+//    let mut new_scale = vec![];
+//
+//    for (i, degree) in scale.iter().enumerate() {
+//        let distance = SCALE_DEGREES[i];
+//
+//        if distance == 0 {
+//            new_scale.push(root_note.clone());
+//        }
+//    }
+//
+//    new_scale
+//}
 
 pub fn scale_from_string(scale: &str) -> Vec<Pitch> {
     let mut result = vec![];
@@ -175,23 +341,19 @@ pub fn scale_from_string(scale: &str) -> Vec<Pitch> {
     result
 }
 
-pub fn display_scale(scale: &[Pitch]) {
-    for pitch in scale {
-        print!("{} ", pitch);
-    }
-
-    println!()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::pitch::PitchClass;
 
     #[test]
     fn compute_scale_test() {
         let a = Pitch::new(PitchClass::A);
-        let a_ionian = compute_scale(&a, &IONIAN);
+        let a_ionian = Scale::new(&a, "ionian", &IONIAN);
 
-        assert_eq!(scale_from_string("A1 B1 C#1 D1 E1 F#1 G#1"), a_ionian);
+        assert_eq!(
+            scale_from_string("A1 B1 C#1 D1 E1 F#1 G#1"),
+            a_ionian.pitches
+        );
     }
 }
